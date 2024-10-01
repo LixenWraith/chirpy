56a974cc0dd2922f23316d88b765a85e6b166545
LixenWraith <lixen.wraith@gmail.com>
2024-10-01 10:28:59 -0400

    0.2.1. metrics / simple middleware added

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handleMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Metrics reset: %d", cfg.fileserverHits.Load())))
+}
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -15,11 +41,17 @@ func main() {
 	const filepath = "./site"
 	const port = "8080"
 	const readinesspath = "/healthz"
+	const metricspath = "/metrics"
+	const metricsreset = "/reset"
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepath))))
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepath)))))
 	mux.HandleFunc(readinesspath, handlerReadiness)
+	mux.HandleFunc(metricspath, apiCfg.handleMetrics)
+	mux.HandleFunc(metricsreset, apiCfg.handleMetricsReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
