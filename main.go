7a8c1214498b771e97fc413bc1ee75e43bd8f9cf
LixenWraith <lixen.wraith@gmail.com>
2024-10-15 11:00:08 -0400

    0.4.2. json api endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -42,12 +43,73 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func handlerValidate(w http.ResponseWriter, r *http.Request) {
+	type input struct {
+		Body string `json:"body"`
+	}
+
+	type error_response struct {
+		Error string `json:"error"`
+	}
+
+	type valid_reponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := input{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't decode", err)
+	}
+
+	const maxChirpyLength = 140
+	if len(params.Body) > maxChirpyLength {
+		respondWithError(w, http.StatusBadRequest, "chirpy looong", nil)
+	}
+
+	respondWithJSON(w, http.StatusOK, valid_reponse{
+		Valid: true,
+	})
+
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
 func main() {
 	const filepath = "./site"
 	const port = "8080"
 	const readinesspath = "GET /api/healthz"
 	const metricspath = "GET /admin/metrics"
 	const metricsreset = "POST /admin/reset"
+	const validatepath = "POST /api/validate_chirp"
 
 	mux := http.NewServeMux()
 
@@ -56,6 +118,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepath)))))
 
 	mux.HandleFunc(readinesspath, handlerReadiness)
+	mux.HandleFunc(validatepath, handlerValidate)
 	mux.HandleFunc(metricspath, apiCfg.handleMetrics)
 	mux.HandleFunc(metricsreset, apiCfg.handleMetricsReset)
 
---
+++ test.sh
@@ -0,0 +1,4 @@
+# api endpoint check
+curl -X POST http://localhost:8080/api/validate_chirp -H "Content-Type: application/json" -d '{"key": "value"}'
+
+curl -X POST http://localhost:8080/api/validate_chirp -H "Content-Type: application/json" -d '{"key": "value"}'
